498d57258cfd6a39bd37ac3e156dadaaaadb6437
ArronZhong <956224926@qq.com>
2023-03-18 08:32:54 +0800

    modify git to p4 symbol

--- zsh-p4.plugin.zsh
+++ zsh-p4.plugin.zsh
@@ -5,28 +5,31 @@
 #   Get 'branch' from p4 client -o: By convention the branch is the third 'level' //<depot>/<product>/<branch>
 #   Get status from p4 opened
 #   Get Remote from p4 sync -n
-#   Use $ZSH_THEME_GIT* settings for a consistent look and feel
+#   Use $ZSH_THEME_P4* settings for a consistent look and feel
 function p4_prompt_info() {
   # If we are not in a p4 client get out
   if ! p4 client -o &> /dev/null; then
     return 0
   fi
+  local prj
+  prj=$(p4 client -o | grep // | head -n 1 | cut -d / -f 5 2> /dev/null) || return 0
   # Get 'Branch' info
   local ref
   ref=$(p4 client -o | grep // | tail -n 1 | cut -d / -f 5 2> /dev/null) || return 0
 
   # Todo: Integrate p4 sync info into upstream
   local upstream
+  upstream=$prj
 
-  echo "${ZSH_THEME_GIT_PROMPT_PREFIX}${ref:gs/%/%%}${upstream:gs/%/%%}$(parse_p4_dirty)${ZSH_THEME_GIT_PROMPT_SUFFIX}"
+  echo "${ZSH_THEME_P4_PROMPT_PREFIX}${ref:gs/%/%%}${upstream:gs/%/%%}$(parse_p4_dirty)${ZSH_THEME_P4_PROMPT_SUFFIX}"
 }
 
 function parse_p4_dirty() {
   local STATUS
   STATUS=$(p4 opened 2> /dev/null | tail -n 1)
   if [[ -n $STATUS ]]; then
-    echo "$ZSH_THEME_GIT_PROMPT_DIRTY"
+    echo "$ZSH_THEME_P4_PROMPT_DIRTY"
   else
-    echo "$ZSH_THEME_GIT_PROMPT_CLEAN"
+    echo "$ZSH_THEME_P4_PROMPT_CLEAN"
   fi
 }
